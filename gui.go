29ff282ce0bce0932578b2bf1b26810b99aa71cd
Andrei Pago <andpago@gmail.com>
2018-10-16 19:01:03 +0300

    basic windows

---
+++ gui.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"image/color"
+)
+
+type GuiWindow interface {
+	Draw(w *pixelgl.Window)
+}
+
+type BaseGuiWindow struct {
+	X, Y, W, H float64
+	Bgcolor color.Color
+	Bordercolor color.Color
+}
+
+type MainWindow struct {
+	BaseGuiWindow
+}
+
+func (m MainWindow) Draw(w *pixelgl.Window) {
+	imd := imdraw.New(nil)
+
+	//draw background
+	imd.Color = m.Bgcolor
+	imd.Push(pixel.V(m.X, m.Y))
+	imd.Push(pixel.V(m.X + m.W, m.Y))
+	imd.Push(pixel.V(m.X + m.W, m.Y + m.H))
+	imd.Push(pixel.V(m.X, m.Y + m.H))
+	imd.Polygon(0)
+
+	// draw border
+	imd.Color = m.Bordercolor
+	imd.Push(pixel.V(m.X, m.Y))
+	imd.Push(pixel.V(m.X + m.W, m.Y))
+	imd.Push(pixel.V(m.X + m.W, m.Y + m.H))
+	imd.Push(pixel.V(m.X, m.Y + m.H))
+	imd.Polygon(1)
+
+	imd.Draw(w)
+}
--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
+	"image/color"
 )
 
+
+var windowConfig = pixelgl.WindowConfig{
+	Title:  "Mrak",
+	Bounds: pixel.R(0, 0, 800, 500),
+	VSync:  true,
+}
+
+var w = MainWindow{
+	BaseGuiWindow{
+		W: windowConfig.Bounds.W(),
+		H: windowConfig.Bounds.H(),
+		X: 0,
+		Y: 0,
+		Bgcolor: color.RGBA{128, 128, 128, 255},
+		Bordercolor: color.RGBA{0, 0, 0, 255},
+	},
+}
+
 func run() {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1024, 768),
-		VSync:  true,
-	}
-	win, err := pixelgl.NewWindow(cfg)
+	win, err := pixelgl.NewWindow(windowConfig)
 	if err != nil {
 		panic(err)
 	}
 
 	win.Clear(colornames.Skyblue)
+	w.Draw(win)
 
 	for !win.Closed() {
 		win.Update()
