122f3b35ad992b8d7b200312050e9ff029eb96ee
Andrei Pago <andpago@gmail.com>
2018-10-16 19:26:12 +0300

    z-index

--- gui.go
+++ gui.go
@@ -5,9 +5,11 @@ import (
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"image/color"
+	"sort"
 )
 
 type GuiWindow interface {
+	GetZindex() int
 	Draw(w *pixelgl.Window)
 }
 
@@ -15,6 +17,11 @@ type BaseGuiWindow struct {
 	X, Y, W, H float64
 	Bgcolor color.Color
 	Bordercolor color.Color
+	Zindex int
+}
+
+func (b BaseGuiWindow) GetZindex() int {
+	return b.Zindex
 }
 
 type MainWindow struct {
@@ -41,4 +48,44 @@ func (m MainWindow) Draw(w *pixelgl.Window) {
 	imd.Polygon(1)
 
 	imd.Draw(w)
+}
+
+type Compositor struct {
+	Zbuffer map[int][]GuiWindow
+	win *pixelgl.Window
+}
+
+func (c *Compositor) AddWindow(w GuiWindow) {
+	z := w.GetZindex()
+
+	if _, ok := c.Zbuffer[z]; ok {
+		c.Zbuffer[z] = append(c.Zbuffer[z], w)
+	} else {
+		c.Zbuffer[z] = []GuiWindow{w}
+	}
+}
+
+func (c *Compositor) DrawAllWindows() {
+	zindices := make([]int, len(c.Zbuffer), len(c.Zbuffer))
+
+	i := 0
+	for idx := range c.Zbuffer {
+		zindices[i] = idx
+		i++
+	}
+
+	sort.Ints(zindices)
+
+	for _, idx := range zindices {
+		for _, window := range c.Zbuffer[idx] {
+			window.Draw(c.win)
+		}
+	}
+}
+
+func NewCompositor(win *pixelgl.Window) Compositor {
+	return Compositor {
+		Zbuffer: map[int][]GuiWindow{},
+		win: win,
+	}
 }
--- main.go
+++ main.go
@@ -14,25 +14,55 @@ var windowConfig = pixelgl.WindowConfig{
 	VSync:  true,
 }
 
-var w = MainWindow{
-	BaseGuiWindow{
-		W: windowConfig.Bounds.W(),
-		H: windowConfig.Bounds.H(),
-		X: 0,
-		Y: 0,
-		Bgcolor: color.RGBA{128, 128, 128, 255},
-		Bordercolor: color.RGBA{0, 0, 0, 255},
-	},
-}
-
 func run() {
+
 	win, err := pixelgl.NewWindow(windowConfig)
 	if err != nil {
 		panic(err)
 	}
 
 	win.Clear(colornames.Skyblue)
-	w.Draw(win)
+
+	comp := NewCompositor(win)
+
+	comp.AddWindow(MainWindow{
+		BaseGuiWindow{
+			W: windowConfig.Bounds.W(),
+			H: windowConfig.Bounds.H() - 100,
+			X: 0,
+			Y: 50,
+			Bgcolor: color.RGBA{128, 128, 128, 255},
+			Bordercolor: color.RGBA{0, 0, 0, 255},
+			Zindex: 1,
+		},
+	})
+
+	comp.AddWindow(MainWindow{
+		BaseGuiWindow{
+			W: windowConfig.Bounds.W() - 300,
+			H: windowConfig.Bounds.H(),
+			X: 200,
+			Y: 0,
+			Bgcolor: color.RGBA{255, 255, 255, 255},
+			Bordercolor: color.RGBA{0, 0, 0, 255},
+			Zindex: -3,
+		},
+	})
+
+	comp.AddWindow(MainWindow{
+		BaseGuiWindow{
+			W: windowConfig.Bounds.W() - 500,
+			H: windowConfig.Bounds.H() / 10,
+			X: 500,
+			Y: 0,
+			Bgcolor: color.RGBA{255, 0, 0, 128},
+			Bordercolor: color.RGBA{0, 255, 0, 255},
+			Zindex: 0,
+		},
+	})
+
+
+	comp.DrawAllWindows()
 
 	for !win.Closed() {
 		win.Update()
